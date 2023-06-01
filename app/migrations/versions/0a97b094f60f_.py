e0ecf81688ae418069460436f7482c90be6d1302
[sharoun <[madoyasharoun@gmail.com]>
2023-06-01 13:15:49 +0300

    trials

---
+++ app/migrations/versions/0a97b094f60f_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 0a97b094f60f
+Revises: 02db96a17970
+Create Date: 2023-06-01 12:54:16.964620
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0a97b094f60f'
+down_revision = '02db96a17970'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
